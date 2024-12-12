13e2f655b2763fd8d2d0f58a187475049d478c70
mardijker <me@zef.my.id>
2024-12-12 22:39:22 +0700

    Initialising with Spring Boot

---
+++ src/main/java/com/telu_pjj/manajemen_klinik_hewan/ManajemenKlinikHewanApplication.java
@@ -0,0 +1,13 @@
+package com.telu_pjj.manajemen_klinik_hewan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManajemenKlinikHewanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManajemenKlinikHewanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telu_pjj/manajemen_klinik_hewan/ManajemenKlinikHewanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telu_pjj.manajemen_klinik_hewan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManajemenKlinikHewanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
